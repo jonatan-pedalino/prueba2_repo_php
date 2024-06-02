9fd45da9ee6a48c0350e2bc4b81d21b95580dd80
jonatan <jonatanpedalino133@gmail.com>
2024-06-02 12:15:37 -0300

    entrega

--- inicio_secc/script.js
+++ inicio_secc/script.js
@@ -1,12 +1,25 @@
-document.getElementById("formulario").addEventListener("submit", function(event) {
-    var usuario = document.getElementById("usuario").value;
-    if (usuario == "") {
-        alert("Por favor, completa el campo de email.");
-        event.preventDefault(); // Evita que el formulario se envíe
-    }
-    var password = document.getElementById("password").value;
-    if (password == "") {
-        alert("Por favor, completa el campo de password.");
-        event.preventDefault(); // Evita que el formulario se envíe
-    }
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevenir el envío del formulario
+    validarFormulario();
 });
+
+function validarFormulario() {
+    var email = document.getElementById('email').value.trim();
+    var password = document.getElementById('password').value.trim();
+
+    if (email === '' || password === '') {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Todos los campos son obligatorios',
+        });
+    } else {
+        Swal.fire({
+            icon: 'success',
+            title: 'Éxito',
+            text: 'Ingreso correcto',
+        }).then(() => {
+             document.getElementById('loginForm').submit();
+        });
+    }
+}
